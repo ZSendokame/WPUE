72288ace6c8c108bd54e47dd633a0f9ab0c718ee
ZSendokame <70088953+ZSendokame@users.noreply.github.com>
2022-05-07 11:31:05 -0400

    Update wpue.py

--- wpue.py
+++ wpue.py
@@ -5,12 +5,14 @@ sys.argv = sys.argv[1:]
 
 # Error
 if not len(sys.argv):
-    print('[-] Can\'t find any argument'); exit(1)
+    print('[-] Can\'t find any argument')
+    exit(1)
 
 get = requests.get(sys.argv[0] + '/wp-json/wp/v2/users')
 
 if get.status_code >= 400:
-    print(f'[-] The site "{sys.argv[0]}" does not have "/wp-json/wp/v2/users" directory or it\'s not indexable.'); exit(1)
+    print(f'[-] The site "{sys.argv[0]}" does not have "/wp-json/wp/v2/users" directory or it\'s not indexable.')
+    exit(1)
 
 # Main
 print('_______')
