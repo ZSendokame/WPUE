df2fb613edac05e526b957409477b504b0249eb1
Sendo kame <sendo@sendokame.com>
2022-10-12 10:53:18 -0300

    Source code update.

--- wpue.py
+++ wpue.py
@@ -4,10 +4,6 @@ import arguing
 url = arguing.set('--url', mandatory=True, help_message='URL to check.')
 
 # Error
-if not len(arguing.argv):
-    print('[-] Can\'t find any argument')
-    exit(1)
-
 get = requests.get(url + '/wp-json/wp/v2/users')
 
 if get.status_code >= 400:
